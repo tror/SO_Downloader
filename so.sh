4069b56a08341e6866310c1f0c5b49fcaa8ef140
tror <johannes.osterberg@gmail.com>
2017-10-11 14:09:43 +0200

    Clipboard Linux Mac
    Clipboard Linux Mac Phraser for mac and linux

--- so.sh
+++ so.sh
@@ -1,29 +1,37 @@
 #!/bin/bash
-USER="USER"
-PW="password"
+SOID="USER"
+SOPW="PASSWORT"
+
 DWfolder="$HOME/Downloads/so"
 mkdir $DWfolder
 
 cd $DWfolder
 
     echo -e "\033[42mCookie Authenticating....\033[0m"
-	cookie=$(wget --quiet -O- "http://api.share-online.biz/cgi-bin?q=userdetails&username=$USER&password=$PW" | grep 'a=');
+	socookie=$(wget --quiet -O- "http://api.share-online.biz/cgi-bin?q=userdetails&username=$SOID&password=$SOPW" | grep 'a=');	
 	echo -e "\033 Urls in urls.txt\033[0m"
 	cat urls.txt 
 	echo
 	echo -e "\033[41mUrls Reload? j / n \033[0m"
-	read -s urlload
+	read -s urlreload
 
-if [ "$urlload" = "j" ]
+if [ "urlreload" = "j" ]
   then
 	  #pbpast MacOSX only
-	  pbpaste > urls.txt 
+	  
+	  __IS_MAC=${__IS_MAC:-$(test $(uname -s) == "Darwin" && echo 'true')}
+	  if [ -n "${__IS_MAC}" ]; then
+	    pbpaste > urls.txt #For MAC OSX
+	  else
+	    xclip -selection clipboard -o > urls.txt  #For linux
+	  fi
+	  	  
 fi
 
 for url in $(cat urls.txt)
 	do
 		echo -e "\033[42mGeneriere SHARE Link\033[0m"
-		url=$(wget --quiet -O- "http://api.share-online.biz/cgi-bin?q=linkdata&username=$USER&password=$PW&lid=$(basename "$url")" | grep 'URL' | cut -f 2 -d ' ');
+		url=$(wget --quiet -O- "http://api.share-online.biz/cgi-bin?q=linkdata&username=$SOID&password=$SOPW&lid=$(basename "$url")" | grep 'URL' | cut -f 2 -d ' ');
 		echo -e "\033[42mDownload URL \033[0m"$url
-		wget -c  --content-disposition --header "Cookie: $cookie" $url
+		wget -c  --content-disposition --header "Cookie: $socookie" $url
 	done
